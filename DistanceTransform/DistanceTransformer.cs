0caa23c02323062461c781e1e115f57bc406aeb6
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-12-31 11:25:52 +0900

    Classnized distance transformer

---
+++ DistanceTransform/DistanceTransformer.cs
@@ -0,0 +1,73 @@
+﻿using OpenCvSharp;
+
+namespace DistanceTransform;
+
+public static class DistanceTransformer
+{
+    public record DistanceLabel
+    {
+        public int Y { get; }
+        public int X { get; }
+        public int Distance { get; }
+
+        public DistanceLabel(int y, int x, int distance)
+        {
+            Y = y;
+            X = x;
+            Distance = distance;
+        }
+    }
+
+    public static List<DistanceLabel> GetDistanceLabel(Mat binaryImage)
+    {
+        // 距離変換を適用する
+        var dist = binaryImage.DistanceTransform(DistanceTypes.C, DistanceTransformMasks.Mask3);
+        dist.MinMaxLoc(out double _, out var maxVal);
+
+        // 出力画像の準備
+        var output = new List<DistanceLabel>();
+        // 距離値ごとに色を変えて描画する（パラレル処理）
+        Parallel.For(0, (int) maxVal + 1, distance =>
+        {
+            for (var y = 0; y < dist.Rows; y++)
+            for (var x = 0; x < dist.Cols; x++)
+            {
+                if ((int) dist.At<float>(y, x) != distance) continue;
+                output.Add(new DistanceLabel(y, x, distance));
+            }
+        });
+
+        return output;
+    }
+
+    public static void ShowDistanceColor(Mat binaryImage)
+    {
+        // 距離変換を適用する
+        var dist = binaryImage.DistanceTransform(DistanceTypes.C, DistanceTransformMasks.Mask3);
+        dist.MinMaxLoc(out double _, out var maxVal);
+
+        // 出力画像の準備
+        var output = binaryImage.CvtColor(ColorConversionCodes.GRAY2BGR);
+        var random = new Random();
+
+        // 距離値ごとに色を変えて描画する（パラレル処理）
+        Parallel.For(0, (int) maxVal + 1, distance =>
+        {
+            var color = new Vec3b(
+                (byte) random.Next(256),
+                (byte) random.Next(256),
+                (byte) random.Next(256));
+
+            for (var y = 0; y < dist.Rows; y++)
+            for (var x = 0; x < dist.Cols; x++)
+            {
+                if ((int) dist.At<float>(y, x) != distance) continue;
+                output.Set(y, x, color);
+            }
+        });
+
+        // 結果を表示する
+        Cv2.ImShow("Color Coded Distance Map", output);
+        Cv2.WaitKey(0);
+    }
+}
--- DistanceTransform/Program.cs
+++ DistanceTransform/Program.cs
@@ -1,39 +1,32 @@
-﻿using InsideContours;
+﻿using DistanceTransform;
+using InsideContours;
 using OpenCvSharp;
 
 const int w = 640;
 const int h = 480;
 var points = ShapeCreator.CalculatePolygonPoints(5, w / 2, h / 2, h / 4);
-//using var src = ShapeCreator.DrawTrapezoid(w, h);
+using var src = ShapeCreator.DrawTrapezoid(w, h);
 //using var src = ShapeCreator.DrawEllipse(w, h);
-using var src = ShapeCreator.DrawPolygon(w, h, points);
+//using var src = ShapeCreator.DrawPolygon(w, h, points);
 var contours = src.FindContoursAsArray(RetrievalModes.External, ContourApproximationModes.ApproxNone);
 var binaryImage = src.Clone();
 Cv2.DrawContours(binaryImage, contours, -1, new Scalar(255), -1);
 
-// 距離変換を適用する
-var dist = binaryImage.DistanceTransform(DistanceTypes.C, DistanceTransformMasks.Mask3);
-dist.MinMaxLoc(out double _, out var maxVal);
+//DistanceTransformer.ShowDistanceColor(binaryImage);
 
-// 出力画像の準備
-var output = binaryImage.CvtColor(ColorConversionCodes.GRAY2BGR);
-var random = new Random();
-// 距離値ごとに色を変えて描画する（パラレル処理）
-Parallel.For(0, (int) maxVal + 1, distance =>
+var distanceLabels = DistanceTransformer.GetDistanceLabel(binaryImage);
+
+var maxDistance = distanceLabels.Max(x => x.Distance);
+for (var i = 0; i < maxDistance - 1; i++)
 {
-    var color = new Vec3b(
-        (byte) random.Next(256),
-        (byte) random.Next(256),
-        (byte) random.Next(256));
-
-    for (var y = 0; y < dist.Rows; y++)
-    for (var x = 0; x < dist.Cols; x++)
+    var arr = distanceLabels.Where(x => x.Distance == i).ToArray();
+    var output = new Mat(h, w, MatType.CV_8UC3);
+    foreach (var distanceLabel in arr)
     {
-        if ((int) dist.At<float>(y, x) != distance) continue;
-        output.Set(y, x, color);
+        Cv2.Circle(output, new Point(distanceLabel.X, distanceLabel.Y), 1, Scalar.Red, -1);
     }
-});
 
-// 結果を表示する
-Cv2.ImShow("Color Coded Distance Map", output);
-Cv2.WaitKey(0);
+    Cv2.ImShow($"Distance: {i}", output);
+    Cv2.WaitKey(100);
+    Cv2.DestroyAllWindows();
+}
